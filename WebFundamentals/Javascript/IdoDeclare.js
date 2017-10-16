006784821a746b5aee36a923e8251a21cbb1dd16
rj-aj <jainreena23@gmail.com>
2017-10-16 15:59:33 -0400

    dojo assigmnets -html css javascript1

---
+++ WebFundamentals/Javascript/IdoDeclare.js
@@ -0,0 +1,25 @@
+var num1 = 1;
+var num2 = -22;
+var num3 = 3.56;
+var num4 = 4233;
+
+var string1="The First Sting";
+var string2="The second Sting";
+var string3="The third Sting";
+
+var boolean1 =true;
+var boolean2 = false;
+
+var num5;
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(boolean1);
+console.log(boolean2);
+console.log(num5);
